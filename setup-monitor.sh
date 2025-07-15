efc6467fb1cf8b1029de0957fa7b8ede89ce2776
Lazy Cat <153988610+beingyxq@users.noreply.github.com>
2025-07-16 07:44:00 +0800

    Update setup-monitor.sh

--- setup-monitor.sh
+++ setup-monitor.sh
@@ -29,7 +29,7 @@ mkdir -p /etc/ak_monitor/
 cd /etc/ak_monitor/
 
 # Download monitor
-wget -O ak_monitor https://p.221320.xyz/https://github.com/akile-network/akile_monitor/releases/latest/download/$MONITOR_FILE
+wget -O ak_monitor https://github.com/akile-network/akile_monitor/releases/latest/download/$MONITOR_FILE
 chmod 777 ak_monitor
 
 # Create service file
